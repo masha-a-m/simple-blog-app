ac601e72294b0943c90fe8834d736ffe31f5d802
masha-a-m <escritoramaree@gmail.com>
2024-09-09 20:42:03 +0800

    edits

--- myblog/settings.py
+++ myblog/settings.py
@@ -153,4 +153,4 @@ MEDIA_ROOT = BASE_DIR/'media/pics'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
