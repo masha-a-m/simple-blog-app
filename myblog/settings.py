553a3a928b0466ad803fa41cd513be173c7e91af
masha-a-m <escritoramaree@gmail.com>
2024-09-11 03:07:45 +0800

    addd

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1:8001', 'simpleblogapp-d7dbdee0e375.herokuapp.com']
 
