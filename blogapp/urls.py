e0773a6f6e327c18b413dd2104bab23686a0f36c
masha-a-m <escritoramaree@gmail.com>
2024-09-12 04:31:13 +0800

    addd

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-
-    # path('readmore/<str:slug>', views.readmore, name = 'readmore'),
     path('blogapp/<str:slug>', views.readmore, name = 'readmore')
 ]
